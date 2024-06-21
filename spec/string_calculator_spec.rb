17b09a0f56e89bdfb43202f09647b085e0f87021
chirag garg <chiragagrawal2906.com>
2024-06-22 00:43:10 +0530

    Handle New Lines Between Numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,21 +1,27 @@
 require './string_calculator'
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+  
   describe '.add' do
     it 'returns 0 for an empty string' do
-      expect(StringCalculator.add("")).to eq(0)
+      expect(calculator.add("")).to eq(0)
     end
 
     it 'returns the number for a single number' do
-      expect(StringCalculator.add("1")).to eq(1)
+      expect(calculator.add("1")).to eq(1)
     end
 
     it 'returns the sum of two numbers' do
-      expect(StringCalculator.add("1,5")).to eq(6)
+      expect(calculator.add("1,5")).to eq(6)
     end
 
     it 'returns the sum of multiple numbers' do
-      expect(StringCalculator.add("1,2,3")).to eq(6)
+      expect(calculator.add("1,2,3")).to eq(6)
+    end
+
+    it 'handles new lines between numbers' do
+      expect(calculator.add("1\n2,3")).to eq(6)
     end
   end
 end
